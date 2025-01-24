2327658c17fa221768ed52002b2ecf7323eb6007
Alexandr <aleksandrivanov20208@gmail.com>
2025-01-24 17:11:54 +0400

    Добавил более менее адекватный интерфейс

---
+++ FileO/FileO/LBItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileO
+{
+    internal class LBItem
+    {
+        public string Name { get; set; }
+        public string IconPath {  get; set; }
+
+        public LBItem(string name, string iconPath)
+        {
+            Name = name;
+            IconPath = iconPath;
+        }
+    }
+}
